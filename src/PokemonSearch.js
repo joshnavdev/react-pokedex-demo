f66bf9c99c9ab0f750f39e259e557dd9f2aa6fd2
Joshua Navarro <joshua.navarro35@gmail.com>
2021-02-07 03:54:05 -0500

    Improve styles

--- src/PokemonSearch.js
+++ src/PokemonSearch.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import classNames from 'classnames';
+
 function PokemonSearch(props) {
+  const [isHidden, setIsHidden] = useState(false);
   const { search, onSearch, onClick } = props;
+  const buttonStyle = classNames({
+    'SearchButton': true,
+    'isHidden': search === '',
+  });
+
   return (
     <div className="Search">
       <input className="SearchInput" value={search} onChange={onSearch} />
-      <button className="SearchButton" onClick={onClick}>X</button>
+      <button className={buttonStyle} onClick={onClick}>X</button>
     </div>
   )
 }
